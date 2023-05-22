bf3b73c8885333a71de6a8bac94a5c1e738921ac
Sherman Rofeman <skippyr.developer@gmail.com>
2023-05-22 10:32:04 -0300

    refactor: reduce unecessary directory structure

--- nvim/lua/settings.lua
+++ nvim/lua/settings.lua
@@ -8,9 +8,10 @@ vim.opt.relativenumber = true
 vim.opt.tabstop = tab_size_in_spaces
 vim.opt.shiftwidth = tab_size_in_spaces
 vim.opt.expandtab = false
-vim.opt.list = true
 vim.opt.laststatus = 1
+vim.opt.list = true
 vim.opt.listchars = hidden_characters
 vim.opt.guicursor = ""
+vim.cmd("filetype plugin off")
 vim.cmd("colorscheme flamerial")
 
